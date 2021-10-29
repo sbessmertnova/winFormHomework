ff90b11a1bb8049939a7d4a74d2f9414c3ac80bc
sbessmertnova <minnij@yandex.ru>
2021-10-29 18:40:24 +0300

    added logic

--- Lesson7-2/GuessNumber.Designer.cs
+++ Lesson7-2/GuessNumber.Designer.cs
@@ -32,9 +32,9 @@ namespace Lesson7_2
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.GuessNumberHeaderLabel = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.GuessedNumberTextLabel = new System.Windows.Forms.TextBox();
-            this.CheckGuessedNumberButton = new System.Windows.Forms.Button();
             this.GuessedNumberResultLabel = new System.Windows.Forms.Label();
+            this.CheckGuessedNumberButton = new System.Windows.Forms.Button();
+            this.GuessedNumberTextLabel = new System.Windows.Forms.TextBox();
             this.tableLayoutPanel1.SuspendLayout();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -49,12 +49,13 @@ namespace Lesson7_2
             this.tableLayoutPanel1.Controls.Add(this.panel1, 1, 1);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 3;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 70F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(849, 295);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1698, 567);
             this.tableLayoutPanel1.TabIndex = 0;
             this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
             // 
@@ -62,9 +63,10 @@ namespace Lesson7_2
             // 
             this.GuessNumberHeaderLabel.AutoSize = true;
             this.GuessNumberHeaderLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.GuessNumberHeaderLabel.Location = new System.Drawing.Point(130, 0);
+            this.GuessNumberHeaderLabel.Location = new System.Drawing.Point(260, 0);
+            this.GuessNumberHeaderLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.GuessNumberHeaderLabel.Name = "GuessNumberHeaderLabel";
-            this.GuessNumberHeaderLabel.Size = new System.Drawing.Size(182, 31);
+            this.GuessNumberHeaderLabel.Size = new System.Drawing.Size(365, 63);
             this.GuessNumberHeaderLabel.TabIndex = 0;
             this.GuessNumberHeaderLabel.Text = "Угадай число";
             // 
@@ -74,46 +76,53 @@ namespace Lesson7_2
             this.panel1.Controls.Add(this.CheckGuessedNumberButton);
             this.panel1.Controls.Add(this.GuessedNumberTextLabel);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel1.Location = new System.Drawing.Point(130, 47);
+            this.panel1.Location = new System.Drawing.Point(260, 91);
+            this.panel1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(588, 200);
+            this.panel1.Size = new System.Drawing.Size(1176, 384);
             this.panel1.TabIndex = 1;
-            // 
-            // GuessedNumberTextLabel
-            // 
-            this.GuessedNumberTextLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.GuessedNumberTextLabel.Location = new System.Drawing.Point(3, 3);
-            this.GuessedNumberTextLabel.Name = "GuessedNumberTextLabel";
-            this.GuessedNumberTextLabel.Size = new System.Drawing.Size(164, 38);
-            this.GuessedNumberTextLabel.TabIndex = 0;
-            // 
-            // CheckGuessedNumberButton
-            // 
-            this.CheckGuessedNumberButton.BackColor = System.Drawing.SystemColors.Highlight;
-            this.CheckGuessedNumberButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CheckGuessedNumberButton.Location = new System.Drawing.Point(173, 3);
-            this.CheckGuessedNumberButton.Name = "CheckGuessedNumberButton";
-            this.CheckGuessedNumberButton.Size = new System.Drawing.Size(174, 38);
-            this.CheckGuessedNumberButton.TabIndex = 1;
-            this.CheckGuessedNumberButton.Text = "Проверить";
-            this.CheckGuessedNumberButton.UseVisualStyleBackColor = false;
             // 
             // GuessedNumberResultLabel
             // 
             this.GuessedNumberResultLabel.AutoSize = true;
             this.GuessedNumberResultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.GuessedNumberResultLabel.Location = new System.Drawing.Point(3, 74);
+            this.GuessedNumberResultLabel.Location = new System.Drawing.Point(6, 142);
+            this.GuessedNumberResultLabel.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.GuessedNumberResultLabel.Name = "GuessedNumberResultLabel";
-            this.GuessedNumberResultLabel.Size = new System.Drawing.Size(86, 31);
+            this.GuessedNumberResultLabel.Size = new System.Drawing.Size(82, 126);
             this.GuessedNumberResultLabel.TabIndex = 2;
-            this.GuessedNumberResultLabel.Text = "label2";
+            this.GuessedNumberResultLabel.Text = " ";
+            // 
+            // CheckGuessedNumberButton
+            // 
+            this.CheckGuessedNumberButton.BackColor = System.Drawing.SystemColors.Highlight;
+            this.CheckGuessedNumberButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CheckGuessedNumberButton.Location = new System.Drawing.Point(346, 6);
+            this.CheckGuessedNumberButton.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.CheckGuessedNumberButton.Name = "CheckGuessedNumberButton";
+            this.CheckGuessedNumberButton.Size = new System.Drawing.Size(348, 73);
+            this.CheckGuessedNumberButton.TabIndex = 1;
+            this.CheckGuessedNumberButton.Text = "Проверить";
+            this.CheckGuessedNumberButton.UseVisualStyleBackColor = false;
+            this.CheckGuessedNumberButton.Click += new System.EventHandler(this.CheckGuessedNumberButton_Click);
+            // 
+            // GuessedNumberTextLabel
+            // 
+            this.GuessedNumberTextLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GuessedNumberTextLabel.Location = new System.Drawing.Point(6, 6);
+            this.GuessedNumberTextLabel.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.GuessedNumberTextLabel.Name = "GuessedNumberTextLabel";
+            this.GuessedNumberTextLabel.Size = new System.Drawing.Size(324, 68);
+            this.GuessedNumberTextLabel.TabIndex = 0;
+            this.GuessedNumberTextLabel.TextChanged += new System.EventHandler(this.GuessedNumberTextLabel_TextChanged);
             // 
             // GuessNumber
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(849, 295);
+            this.ClientSize = new System.Drawing.Size(1698, 567);
             this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "GuessNumber";
             this.Text = "GuessNumber";
             this.tableLayoutPanel1.ResumeLayout(false);
