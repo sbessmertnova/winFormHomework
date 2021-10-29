a9394314f71f26b03e3511ccbae61d6481284310
Вячеслав Алексеев <p572slava@gmail.com>
2021-10-29 15:05:16 +0300

    init + gui

---
+++ Lesson7/CounterControl.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace Lesson7
+{
+    partial class CounterControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnCommand1 = new System.Windows.Forms.Button();
+            this.btnCommand2 = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.labelSum = new System.Windows.Forms.Label();
+            this.labelTextSum = new System.Windows.Forms.Label();
+            this.labelCommandCountText = new System.Windows.Forms.Label();
+            this.labelCommandCount = new System.Windows.Forms.Label();
+            this.btnCancelTurn = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnCancelTurn);
+            this.panel1.Controls.Add(this.btnReset);
+            this.panel1.Controls.Add(this.btnCommand2);
+            this.panel1.Controls.Add(this.btnCommand1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(300, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 500);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnCommand1
+            // 
+            this.btnCommand1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnCommand1.Location = new System.Drawing.Point(3, 6);
+            this.btnCommand1.Name = "btnCommand1";
+            this.btnCommand1.Size = new System.Drawing.Size(194, 80);
+            this.btnCommand1.TabIndex = 0;
+            this.btnCommand1.Text = "+1";
+            this.btnCommand1.UseVisualStyleBackColor = false;
+            // 
+            // btnCommand2
+            // 
+            this.btnCommand2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnCommand2.Location = new System.Drawing.Point(3, 92);
+            this.btnCommand2.Name = "btnCommand2";
+            this.btnCommand2.Size = new System.Drawing.Size(194, 80);
+            this.btnCommand2.TabIndex = 1;
+            this.btnCommand2.Text = "x2";
+            this.btnCommand2.UseVisualStyleBackColor = false;
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.SystemColors.Info;
+            this.btnReset.Location = new System.Drawing.Point(3, 178);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnReset.Size = new System.Drawing.Size(194, 80);
+            this.btnReset.TabIndex = 2;
+            this.btnReset.Text = "Сброс";
+            this.btnReset.UseVisualStyleBackColor = false;
+            // 
+            // labelSum
+            // 
+            this.labelSum.AutoSize = true;
+            this.labelSum.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSum.Location = new System.Drawing.Point(131, 6);
+            this.labelSum.Name = "labelSum";
+            this.labelSum.Size = new System.Drawing.Size(29, 31);
+            this.labelSum.TabIndex = 1;
+            this.labelSum.Text = "0";
+            // 
+            // labelTextSum
+            // 
+            this.labelTextSum.AutoSize = true;
+            this.labelTextSum.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTextSum.Location = new System.Drawing.Point(16, 6);
+            this.labelTextSum.Name = "labelTextSum";
+            this.labelTextSum.Size = new System.Drawing.Size(109, 31);
+            this.labelTextSum.TabIndex = 2;
+            this.labelTextSum.Text = "Сумма:";
+            // 
+            // labelCommandCountText
+            // 
+            this.labelCommandCountText.AutoSize = true;
+            this.labelCommandCountText.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCommandCountText.Location = new System.Drawing.Point(18, 54);
+            this.labelCommandCountText.Name = "labelCommandCountText";
+            this.labelCommandCountText.Size = new System.Drawing.Size(193, 24);
+            this.labelCommandCountText.TabIndex = 3;
+            this.labelCommandCountText.Text = "Количество команд:";
+            // 
+            // labelCommandCount
+            // 
+            this.labelCommandCount.AutoSize = true;
+            this.labelCommandCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCommandCount.Location = new System.Drawing.Point(217, 55);
+            this.labelCommandCount.Name = "labelCommandCount";
+            this.labelCommandCount.Size = new System.Drawing.Size(29, 31);
+            this.labelCommandCount.TabIndex = 4;
+            this.labelCommandCount.Text = "0";
+            // 
+            // btnCancelTurn
+            // 
+            this.btnCancelTurn.BackColor = System.Drawing.SystemColors.Info;
+            this.btnCancelTurn.Location = new System.Drawing.Point(3, 264);
+            this.btnCancelTurn.Name = "btnCancelTurn";
+            this.btnCancelTurn.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnCancelTurn.Size = new System.Drawing.Size(194, 80);
+            this.btnCancelTurn.TabIndex = 3;
+            this.btnCancelTurn.Text = "Отмена хода";
+            this.btnCancelTurn.UseVisualStyleBackColor = false;
+            // 
+            // CounterControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.labelCommandCount);
+            this.Controls.Add(this.labelCommandCountText);
+            this.Controls.Add(this.labelTextSum);
+            this.Controls.Add(this.labelSum);
+            this.Controls.Add(this.panel1);
+            this.Name = "CounterControl";
+            this.Size = new System.Drawing.Size(500, 500);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnCommand2;
+        private System.Windows.Forms.Button btnCommand1;
+        private System.Windows.Forms.Label labelSum;
+        private System.Windows.Forms.Label labelTextSum;
+        private System.Windows.Forms.Label labelCommandCountText;
+        private System.Windows.Forms.Label labelCommandCount;
+        private System.Windows.Forms.Button btnCancelTurn;
+    }
+}
---
+++ Lesson7/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace Lesson7
+{
+    partial class Udvoitel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.toolStripContainer1 = new System.Windows.Forms.ToolStripContainer();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.игратьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.игратьToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.counterControl1 = new Lesson7.CounterControl();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.toolStripContainer1.TopToolStripPanel.SuspendLayout();
+            this.toolStripContainer1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 500F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.toolStripContainer1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.counterControl1, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 500F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.00001F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(646, 603);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // toolStripContainer1
+            // 
+            // 
+            // toolStripContainer1.ContentPanel
+            // 
+            this.toolStripContainer1.ContentPanel.Size = new System.Drawing.Size(67, 21);
+            this.toolStripContainer1.Location = new System.Drawing.Point(3, 3);
+            this.toolStripContainer1.Name = "toolStripContainer1";
+            this.toolStripContainer1.Size = new System.Drawing.Size(67, 45);
+            this.toolStripContainer1.TabIndex = 0;
+            this.toolStripContainer1.Text = "toolStripContainer1";
+            // 
+            // toolStripContainer1.TopToolStripPanel
+            // 
+            this.toolStripContainer1.TopToolStripPanel.Controls.Add(this.menuStrip1);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.игратьToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(67, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // игратьToolStripMenuItem
+            // 
+            this.игратьToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.игратьToolStripMenuItem1});
+            this.игратьToolStripMenuItem.Name = "игратьToolStripMenuItem";
+            this.игратьToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.игратьToolStripMenuItem.Text = "Игра";
+            // 
+            // игратьToolStripMenuItem1
+            // 
+            this.игратьToolStripMenuItem1.Name = "игратьToolStripMenuItem1";
+            this.игратьToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.игратьToolStripMenuItem1.Text = "Играть";
+            // 
+            // counterControl1
+            // 
+            this.counterControl1.Location = new System.Drawing.Point(76, 54);
+            this.counterControl1.Name = "counterControl1";
+            this.counterControl1.Size = new System.Drawing.Size(494, 494);
+            this.counterControl1.TabIndex = 1;
+            // 
+            // Udvoitel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 603);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Udvoitel";
+            this.Text = "Удвоитель";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.ResumeLayout(false);
+            this.toolStripContainer1.TopToolStripPanel.PerformLayout();
+            this.toolStripContainer1.ResumeLayout(false);
+            this.toolStripContainer1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.ToolStripContainer toolStripContainer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem игратьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem игратьToolStripMenuItem1;
+        private CounterControl counterControl1;
+    }
+}
+
