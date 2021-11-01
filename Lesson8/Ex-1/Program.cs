5845316f1a0aade16336c7ae3a7c5363aa0f1581
sbessmertnova <minnij@yandex.ru>
2021-11-01 15:34:29 +0300

    added first ex.

---
+++ Lesson8/Ex-1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_1
+{
+    //Бессмертнова
+    //Задание1. С помощью рефлексии выведите все свойства структуры DateTime.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type type = typeof(DateTime);
+            PropertyInfo[] propertiesDateTime = type.GetProperties();
+            Console.WriteLine("Свойства структуры DateTime:");
+            foreach (var property in propertiesDateTime)
+            {
+                Console.WriteLine($"{property.Name}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
